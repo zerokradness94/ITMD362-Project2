269eb4c66ae5fe76773bba9150017203516b4db6
jknific719 <jknific719@gmail.com>
2017-12-07 21:04:18 -0600

    Removed non-functioning Javascript
    Removed javascript that was not properly functioning and could not be gotten to properly function

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-//Replaces placeholder CWID with defined CWID
-var cwid = "A2982319873";
-var pin = 342587;
-window.onload = function() {
-  document.getElementById('cwid').innerHTML = "CWID: " + cwid + " (Alt. Pin: " + pin + ")";
-  document.getElementById("pinterm").addEventListener('submit', validateForm);
-}
-/*
-function validateForm(event) {
-  var id = document.pinterm.pin.value;
-  if (id != pin || 0) {
-    alert("Error: Incorrect PIN / No PIN entered");
-  }
-}*/
-
 function buttonSuccess()
 {
   var a=document.forms["AddDrop"]["crn1_field"].value;
@@ -21,7 +6,7 @@ function buttonSuccess()
   var d=document.forms["AddDrop"]["crn4_field"].value;
   var e=document.forms["AddDrop"]["crn5_field"].value;
   var f=document.forms["AddDrop"]["crn6_field"].value;
-  
+
 if (a !== "29744" && a !== "28987" && a !== "29690" && a !== "20574" &&
 b !== "29744" && b !== "28987" && b !== "29690" && b !== "20574" &&
 c !== "29744" && c !== "28987" && c !== "29690" && c !== "20574" &&
@@ -32,7 +17,7 @@ f !== "29744" && f !== "28987" && f !== "29690" && e !== "20574" )
     alert('Must enter a valid CRN number');
 	return false;
   }
-else 
+else
  {
    alert('Classes have been added successfully');
    return false;
